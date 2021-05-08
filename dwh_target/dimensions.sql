79b70508e1800cb9d439019c1f5d5938cb405b5f
Anna Urbala <aurbala@gmail.com>
2021-05-08 12:07:09 +0200

    fix

--- dwh_target/dimensions.sql
+++ dwh_target/dimensions.sql
@@ -1,5 +1,6 @@
 USE projekt_target;
 
+
 DROP TABLE IF EXISTS dbo.DistrictDimension;
 DROP TABLE IF EXISTS dbo.VendorDimension;
 DROP TABLE IF EXISTS dbo.AddressDimension;
@@ -29,12 +30,12 @@ CREATE TABLE DistrictDimension (
 	districtId int IDENTITY(1, 1) PRIMARY KEY,
 	originalDistrictId int NOT NULL,
 	name varchar(31) NOT NULL,
-	in_warsaw_date int NOT NULL foreign key references DateDimension(DateID),
-	area_total varchar(10) NOT NULL, --[0,16) mala, [16, 30) srednia, [30,) duza, unknown 
+	inWarsawDate int NOT NULL foreign key references DateDimension(DateID),
+	areaTotal varchar(10) NOT NULL, --[0,16) mala, [16, 30) srednia, [30,) duza, unknown 
 	-- population_total int NULL, --proponuje to jednak usunac, sama populacja w oderwaniu od powierzchni absolutnie nic nie mowi, gestosc powinna wystarczyc
-	population_dens varchar(1) NOT NULL, -- [0,1300) mala, [1300, 6000) srednia, [6000,) duza, unknown 
+	populationDens varchar(1) NOT NULL, -- [0,1300) mala, [1300, 6000) srednia, [6000,) duza, unknown 
 	citypart varchar(20) NOT NULL, --lewo/prawobrzezna
-	district_office_address int NOT NULL foreign key references AddressDimension(addressId),
+	districtOfficeAddress int NOT NULL foreign key references AddressDimension(addressId),
 	validFrom int NOT NULL foreign key references DateDimension(DateID),
 	validTo int NOT NULL foreign key references DateDimension(DateID)
 );
