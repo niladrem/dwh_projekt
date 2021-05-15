a41e17e0b759a395473c1b294417a970897b2d38
Anna Urbala <aurbala@gmail.com>
2021-05-15 16:23:28 +0200

    extra rows

--- dwh_target/dimensions.sql
+++ dwh_target/dimensions.sql
@@ -8,10 +8,10 @@ DROP TABLE IF EXISTS dbo.AddressDimension;
 CREATE TABLE AddressDimension (
 	addressId int IDENTITY(1, 1) PRIMARY KEY,
 	apartmentNumber varchar(30) NOT NULL,
-	street varchar(50) NOT NULL,
+	street varchar(100) NOT NULL,
 	province varchar(30) NOT NULL,
-	postcode varchar(6),
-	city varchar(20)
+	postcode varchar(6) NOT NULL,
+	city varchar(20) NOT NULL
 );
 
 CREATE TABLE VendorDimension (
@@ -33,9 +33,15 @@ CREATE TABLE DistrictDimension (
 	inWarsawDate int NOT NULL foreign key references DateDimension(DateID),
 	areaTotal varchar(10) NOT NULL, --[0,16) mala, [16, 30) srednia, [30,) duza, unknown 
 	-- population_total int NULL, --proponuje to jednak usunac, sama populacja w oderwaniu od powierzchni absolutnie nic nie mowi, gestosc powinna wystarczyc
-	populationDens varchar(1) NOT NULL, -- [0,1300) mala, [1300, 6000) srednia, [6000,) duza, unknown 
+	populationDens varchar(10) NOT NULL, -- [0,1300) mala, [1300, 6000) srednia, [6000,) duza, unknown 
 	citypart varchar(20) NOT NULL, --lewo/prawobrzezna
 	districtOfficeAddress int NOT NULL foreign key references AddressDimension(addressId),
 	validFrom int NOT NULL foreign key references DateDimension(DateID),
 	validTo int NOT NULL foreign key references DateDimension(DateID)
 );
+
+insert into DateDimension values (0, '1900-01-01', 0, '', 0, '', 0, 0, '', 0,0,0,0,0,0,'',0,'',0,'','','1900-01-01','1900-01-01','1900-01-01','1900-01-01','1900-01-01','1900-01-01','1900-01-01','1900-01-01');
+SET IDENTITY_INSERT AddressDimension ON;
+insert into AddressDimension (addressId, apartmentNumber, street, province, postcode, city) values (0, '', '', '','','');
+SET IDENTITY_INSERT AddressDimension OFF;
+insert into VendorDimension values ('', 'o.prywatna', '', 0, 0, 0, 20991231);
